03484ac6041709ed6b9e510abb16c35762cb0b54
Zemlin9239 <48842415+Zemlin9239@users.noreply.github.com>
2020-07-03 01:16:37 +0300

    asd

---
+++ build/precache-manifest.7a502c1ae88d333a5a0d9de4646e832f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6f9c6ed9c143ae32096821ae5f84e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c33bd7803aa160a5ffd0",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d6904fde135c66224791",
+    "url": "/static/css/main.2a317ba8.chunk.css"
+  },
+  {
+    "revision": "c33bd7803aa160a5ffd0",
+    "url": "/static/js/2.e3206038.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e3206038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6904fde135c66224791",
+    "url": "/static/js/main.930be22d.chunk.js"
+  },
+  {
+    "revision": "17066bf72122ad5511d6",
+    "url": "/static/js/runtime-main.8d40984c.js"
+  }
+]);
